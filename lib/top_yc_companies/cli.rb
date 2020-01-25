b60f3ed5fb05d5b4b5700f22e7f2d6d53e07e9d3
favorable-device-5158 <blee2125@gmail.com>
2020-01-25 18:54:26 +0000

    added list_companies method

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -1,6 +1,20 @@
 class TopYcCompanies::CLI
-  
+
   def call
       puts "call method works"
+      list_companies
   end
+
+  def list_companies
+    puts "choose from the list of companies:"
+    puts "  1-10, 11-20, 21-30, 31-40, 41-50,"
+    puts "  51-60, 61-70, 71-80, 81-90, 91-100?"
+    #input= gets.strip.to_i
+
+  end
+
+  def print_company(company)
+
+  end
+
 end

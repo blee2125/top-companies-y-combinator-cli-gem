b87429bd95ced1f535629b2746844357df0d9f6a
favorable-device-5158 <blee2125@gmail.com>
2020-01-25 18:09:58 +0000

    added the required require pathways

--- lib/top_yc_companies.rb
+++ lib/top_yc_companies.rb
@@ -1,3 +1,6 @@
-
+require 'nokogiri'
+require 'open-uri'
 require_relative "./top_yc_companies/version"
 require_relative "./top_yc_companies/cli"
+require_relative "./top_yc_companies/company"
+require_relative "./top_yc_companies/scraper"

e6edf78bf84e1bab93214c2e978e42e93fc4f8b3
favorable-device-5158 <blee2125@gmail.com>
2020-01-25 20:55:02 +0000

    added print_company method

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -1,12 +1,12 @@
 class TopYcCompanies::CLI
 
   def call
-      puts "call method works"
+      puts "TOP COMPANIES FROM Y COMBINATOR"
       list_companies
   end
 
   def list_companies
-    puts "choose from the list of companies:"
+    puts "Choose from 1 - 10 to view a list of companies:"
     puts "  (1)1-10, (2)11-20, (3)21-30, (4)31-40, (5)41-50,"
     puts "  (6)51-60, (7)61-70, (8)71-80, (9)81-90, (10)91-100?"
     puts "  Type 'exit' to quit."
@@ -16,9 +16,9 @@ class TopYcCompanies::CLI
     if input.to_i.between?(1, 10)
       puts "good choice"
     elsif input.downcase == "exit"
-        puts "goodbye"
+        puts "Have a nice day!"
     else
-      puts "try again"
+      puts "Try again!"
       list_companies
     end
 
@@ -30,14 +30,14 @@ class TopYcCompanies::CLI
 
   end
 
-#  def print_company(company)
-#    puts "Company Name: "
-#    puts "Headquarters: "
-#    puts "Founded By: "
-#    puts "Website: "
-#    puts "Sector: "
-#    puts "Overview: "
-#    puts "Apply For A Job: "
-#  end
+ def print_company(company)
+   puts "Company Name: #{company.name}"
+   puts "Headquarters: #{company.hq}"
+   puts "Founded By: #{company.founder}"
+   puts "Website: #{company.website}"
+   puts "Sector: #{company.sector}"
+   puts "Overview: #{company.overview}"
+   puts "Apply For A Job: #{company.jobs_url}"
+ end
 
 end

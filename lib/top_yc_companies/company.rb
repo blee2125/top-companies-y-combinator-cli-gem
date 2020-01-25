2baced26b2ba57583aaa602a1015db198cf15809
favorable-device-5158 <blee2125@gmail.com>
2020-01-25 22:37:27 +0000

    started the methods for different data

--- lib/top_yc_companies/company.rb
+++ lib/top_yc_companies/company.rb
@@ -6,4 +6,28 @@ class TopYcCompanies::Company
 
   end
 
+  def self.all
+    @@all
+  end
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
+  def hq
+    @hq ||=
+  end
+  def founder
+
+  end
+  def website
+
+  end
+  def sector
+
+  end
+  def overview
+
+  end
+  def jobs_url
+
+  end
 end

91e23c0304586766c2995481b3c78951b9e57877
favorable-device-5158 <blee2125@gmail.com>
2020-01-25 21:45:10 +0000

    changed list_companies to list all companies

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -6,14 +6,14 @@ class TopYcCompanies::CLI
   end
 
   def list_companies
-    puts "Choose from 1 - 10 to view a list of companies:"
-    puts "  (1)1-10, (2)11-20, (3)21-30, (4)31-40, (5)41-50,"
-    puts "  (6)51-60, (7)61-70, (8)71-80, (9)81-90, (10)91-100?"
+    puts "Choose from 1 - 100 to view a list of companies:"
+#    puts "  (1)1-10, (2)11-20, (3)21-30, (4)31-40, (5)41-50,"
+#    puts "  (6)51-60, (7)61-70, (8)71-80, (9)81-90, (10)91-100?"
     puts "  Type 'exit' to quit."
 
     input = gets.strip
 
-    if input.to_i.between?(1, 10)
+    if input.to_i.between?(1, 100)
       puts "good choice"
     elsif input.downcase == "exit"
         puts "Have a nice day!"
@@ -30,14 +30,14 @@ class TopYcCompanies::CLI
 
   end
 
- def print_company(company)
-   puts "Company Name: #{company.name}"
-   puts "Headquarters: #{company.hq}"
-   puts "Founded By: #{company.founder}"
-   puts "Website: #{company.website}"
-   puts "Sector: #{company.sector}"
-   puts "Overview: #{company.overview}"
-   puts "Apply For A Job: #{company.jobs_url}"
- end
+  def print_company(company)
+    puts "Company Name: #{company.name}"
+    puts "Headquarters: #{company.hq}"
+    puts "Founded By: #{company.founder}"
+    puts "Website: #{company.website}"
+    puts "Sector: #{company.sector}"
+    puts "Overview: #{company.overview}"
+    puts "Apply For A Job: #{company.jobs_url}"
+  end
 
 end

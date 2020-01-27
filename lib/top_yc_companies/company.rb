bc2086ffc8f2ca8a74b20ca5ea91270fb2d83be8
favorable-device-5158 <blee2125@gmail.com>
2020-01-27 19:26:25 +0000

    added company_number

--- lib/top_yc_companies/company.rb
+++ lib/top_yc_companies/company.rb
@@ -2,8 +2,19 @@ class TopYcCompanies::Company
   attr_accessor :name, :hq, :founder, :website, :sector, :overview, :jobs_url
   @@all= []
 
-  def initialize()
+  def initialize(name)
+    @name= name
+    @@all << self
+  end
 
+  def self.new_company(c)
+    self.new(
+    c.css("h4").text
+    )
+  end
+
+  def self.company_number(number)
+    self.all[number-1]
   end
 
   def self.all
@@ -12,22 +23,24 @@ class TopYcCompanies::Company
   def doc
     @doc ||= Nokogiri::HTML(open(self.url))
   end
-  def hq
-    @hq ||=
-  end
-  def founder
 
-  end
-  def website
-
-  end
-  def sector
-
-  end
-  def overview
-
-  end
-  def jobs_url
-
-  end
+  # def hq
+  #   @hq ||=
+  # end
+  #
+  # def founder
+  #
+  # end
+  # def website
+  #
+  # end
+  # def sector
+  #
+  # end
+  # def overview
+  #
+  # end
+  # def jobs_url
+  #
+  # end
 end

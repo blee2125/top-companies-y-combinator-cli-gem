3e273cec6dcddf74d64123a04c14c40943ef6b37
favorable-device-5158 <blee2125@gmail.com>
2020-01-28 13:37:00 +0000

    added elements to scrape

--- lib/top_yc_companies/company.rb
+++ lib/top_yc_companies/company.rb
@@ -1,17 +1,29 @@
 class TopYcCompanies::Company
-  attr_accessor :name, :hq, :founder, :website, :sector, :overview, :jobs_url
+  attr_accessor :name, :hq, :website, :sector, :overview, :jobs_url
   @@all= []
 
-  def initialize(name)
+  def initialize(name=nil, hq=nil, website=nil, sector=nil, overview=nil, jobs_url=nil)
     @name= name
+    @hq= hq
+    @website= website
+    @sector= sector
+    @overview= overview
+    @jobs_url= jobs_url
     @@all << self
   end
 
   def self.new_company(c)
     self.new(
-    c.css("h4").text
-    )
+      c.css("b.h4").text,
+      c.css("td.hq").text,
+      c.css("a").attribute("href").text,
+      c.css("td.sectors").text,
+      c.css("td.td-description p.margin-bottom-22").text,
+      c.css("").text
+      binding.pry
+      )
   end
+
 
   def self.company_number(number)
     self.all[number-1]
@@ -20,27 +32,4 @@ class TopYcCompanies::Company
   def self.all
     @@all
   end
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
-
-  # def hq
-  #   @hq ||=
-  # end
-  #
-  # def founder
-  #
-  # end
-  # def website
-  #
-  # end
-  # def sector
-  #
-  # end
-  # def overview
-  #
-  # end
-  # def jobs_url
-  #
-  # end
 end

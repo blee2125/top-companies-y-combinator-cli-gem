bbc87307422e0bb67b9e9df3822b6c5dfd886c5f
favorable-device-5158 <blee2125@gmail.com>
2020-01-28 22:33:00 +0000

    finished print_company and print_list

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -3,26 +3,17 @@ class TopYcCompanies::CLI
   def call
     TopYcCompanies::Scraper.new.make_company
     puts "TOP COMPANIES FROM Y COMBINATOR"
-    #greeting
-    binding.pry
+    print_list
     choose_companies
   end
 
-  # def greeting(company)
-  #   puts "TOP COMPANIES FROM Y COMBINATOR"
-  #   Company.all.each do |company|
-  #     puts "#{index}. #{company.name}"
-  #   end
-  # end
-
   def choose_companies
-    puts "Choose from 1 - 100 to view a company:"
+    puts "Choose from 1 - 102 to view a company:"
     puts "  Type 'exit' to quit."
     input = gets.strip
 
-    if input.to_i.between?(1, 100)
+    if input.to_i.between?(1, 102)
       puts "good choice"
-
       company= TopYcCompanies::Company.find(input.to_i)
       print_company(company)
     elsif input.downcase == "exit"
@@ -34,7 +25,14 @@ class TopYcCompanies::CLI
   end
 
   def print_company(company)
-    puts "print company works #{company.name}"
+    puts ""
+    puts "Company Name: #{company.name}"
+    puts "Headquarters: #{company.hq}"
+    puts "Website: #{company.website}"
+    puts "Sector: #{company.sector}"
+    puts "Overview: #{company.overview}"
+    puts "#{company.jobs_url}"
+    puts ""
   end
   # def print_company(company)
   #   puts "Company Name: #{company.name}"
@@ -46,7 +44,7 @@ class TopYcCompanies::CLI
   # end
   def print_list
     TopYcCompanies::Company.all.each.with_index do |c, index|
-      puts "#{index+1}. #{c.name}"
+      puts "#{index+1}.  #{c.name}"
     end
   end
 end

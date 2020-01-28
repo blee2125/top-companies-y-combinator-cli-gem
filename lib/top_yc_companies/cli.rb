4ac2772ae0795a37ccb0837226fcf16dff25fe04
favorable-device-5158 <blee2125@gmail.com>
2020-01-28 13:33:12 +0000

    changed choose_company and added print_list

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -2,7 +2,7 @@ class TopYcCompanies::CLI
 
   def call
       puts "TOP COMPANIES FROM Y COMBINATOR"
-      list_companies
+      choose_companies
   end
 
   # def greeting(company)
@@ -12,10 +12,8 @@ class TopYcCompanies::CLI
   #   end
   # end
 
-  def list_companies
+  def choose_companies
     puts "Choose from 1 - 100 to view a list of companies:"
-#    puts "  (1)1-10, (2)11-20, (3)21-30, (4)31-40, (5)41-50,"
-#    puts "  (6)51-60, (7)61-70, (8)71-80, (9)81-90, (10)91-100?"
     puts "  Type 'exit' to quit."
 
     input = gets.strip
@@ -27,13 +25,8 @@ class TopYcCompanies::CLI
         puts "Have a nice day!"
     else
       puts "Try again!"
-      list_companies
+      choose_companies
     end
-    # while input != "exit"
-    # input= gets.strip.downcase
-    #
-    # end
-    # input= gets.strip.to_i
   end
 
   def print_company
@@ -42,11 +35,14 @@ class TopYcCompanies::CLI
   # def print_company(company)
   #   puts "Company Name: #{company.name}"
   #   puts "Headquarters: #{company.hq}"
-  #   puts "Founded By: #{company.founder}"
   #   puts "Website: #{company.website}"
   #   puts "Sector: #{company.sector}"
   #   puts "Overview: #{company.overview}"
   #   puts "Apply For A Job: #{company.jobs_url}"
   # end
-
+  def print_list
+    TopYcCompanies::Company.all.each.with_index(company) do |company, index|
+      puts "#{index+1}. #{company.name}"
+    end
+  end
 end

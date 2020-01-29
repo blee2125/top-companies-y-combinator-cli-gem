441227cb987e805f6414b39f9b8415320b5e85cc
Brandon Lee <52886743+blee2125@users.noreply.github.com>
2020-01-29 15:07:50 -0500

    add comments

--- lib/top_yc_companies/cli.rb
+++ lib/top_yc_companies/cli.rb
@@ -2,15 +2,17 @@ class TopYcCompanies::CLI
 
   def call
     TopYcCompanies::Scraper.new.make_company
+    puts ""
     puts "TOP COMPANIES FROM Y COMBINATOR"
+    puts ""
     print_list
     choose_companies
   end
 
+  #if statement that allows a user to interact with the program
   def choose_companies
     puts "Choose any number from 1 - 102 to view a company or 'exit' to quit:"
     input = gets.strip
-
     if input.to_i.between?(1, 102)
       company= TopYcCompanies::Company.find(input.to_i)
       print_company(company)
@@ -21,7 +23,8 @@ class TopYcCompanies::CLI
       choose_companies
     end
   end
-
+  
+  #returns information on a company
   def print_company(company)
     puts "================================================"
     puts "Company Name: #{company.name}"
@@ -34,6 +37,7 @@ class TopYcCompanies::CLI
     puts "================================================"
   end
 
+  #returns a list of all companies
   def print_list
     TopYcCompanies::Company.all.each.with_index do |c, index|
       puts "#{index+1}.  #{c.name}"

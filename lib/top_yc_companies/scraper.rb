4113b965e46d4e13a60b442d09f012daf4d75b27
Brandon Lee <52886743+blee2125@users.noreply.github.com>
2020-01-29 14:40:39 -0500

    added comments

--- lib/top_yc_companies/scraper.rb
+++ lib/top_yc_companies/scraper.rb
@@ -1,17 +1,20 @@
 class TopYcCompanies::Scraper
-
+  
+  #retrieves the website to scrape
   def list_page
     Nokogiri::HTML(open("https://www.ycombinator.com/topcompanies/"))
   end
 
+  #scrape_page is responsible for scraping all of the company data
   def scrape_page
     self.list_page.css("tbody#main-companies tr")
   end
-
+  
+  #make_company iterates through each company and passes the data to the new_company method in the Company class
   def make_company
     scrape_page.each do |c|
       TopYcCompanies::Company.new_company(c)
     end
   end
-
+  
 end
